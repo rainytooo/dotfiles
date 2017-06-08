ff618e4b0c2993a9e40abb11db0e09adaf5d94e9
Vincent <ohergal@gmail.com>
2017-06-08 19:03:49 +0800

    fix check oh my zsh installed

--- install/zsh.sh
+++ install/zsh.sh
@@ -5,7 +5,9 @@ echo "Setting up Zsh...\n\n"
 
 if test ! $(which upgrade_oh_my_zsh); then
     # install oh my zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
+    && rm -rf $HOME/.zshrc \
+    && ln -s $HOME/.dotfiles/zsh/zshrc.symlink $HOME/.zshrc 
 else
     upgrade_oh_my_zsh
 fi
