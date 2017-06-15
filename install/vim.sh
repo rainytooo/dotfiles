da7e07bd2ec42d42e6303cab6a18cd0a35ba32d3
Vincent <ohergal@gmail.com>
2017-06-15 11:54:21 +0800

    fix vundle error

--- install/vim.sh
+++ install/vim.sh
@@ -13,6 +13,8 @@ ln -s $HOME/.dotfiles/vim/vim.symlink $HOME/.vim
 
 echo "install vim plugins"
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 vim +PluginInstall +qall
 
 echo "Setup Vim finish...\n\n"
